22c069fe010d7afeb57a5ea55403d69986eb94ee
jealvarez123 <jealvarez123@gmail.com>
2018-03-20 15:33:23 -0500

    added fizzbuzz logic

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,22 @@
 console.log("Up and running!");
+
+
+for ( i=0; i <= 30; i++) {
+
+  if (i % 15 == 0){
+    console.log("fizzbuzz");
+  }
+  else if (i % 5 == 0){
+    console.log("buzz");
+  }
+    else if
+    (i % 3 == 0){
+      console.log("fizz");
+  }
+      else {
+        console.log(i);
+      }
+
+
+
+  }
